72a63c2a498f2a8ad678f5f15f97b70ba92b1da3
Guilherme Jacyzin <jacyzin@gmail.com>
2024-05-23 22:11:50 -0300

    Módulo debugging - parte 3

--- module-debugging/src/CalculadoraMedias.java
+++ module-debugging/src/CalculadoraMedias.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class CalculadoraMedias {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        String[] alunos = {"Camila", "Lucas", "Bruna", "Pedro"};
+
+        int media = calculaMediaDaTuma(alunos, input);
+
+        System.out.printf("Media da turma %d", media);
+
+    }
+
+    public static int calculaMediaDaTuma(String[] alunos, Scanner input) {
+        int soma = 0;
+        for (String aluno : alunos) {
+            System.out.printf("Nota do aluno %s: ", aluno);
+            int nota = input.nextInt();
+            soma += nota;
+        }
+
+        return soma / alunos.length;
 
     }
 }
